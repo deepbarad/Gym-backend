9d48dcaa45531ac5c20faa1b3743ab6bbe03c5f3
deepbarad <deepbarad8@gmail.com>
2024-01-10 16:45:11 +0530

    compleat

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const mongoURL = "mongodb+srv://deepbarad8:5Gdn2P43OepPoHO8@hari-om-gym.xeigk0p.mongodb.net/"
+
 mongoose.set('strictQuery', true);
 const connectToMongo = () => {
     mongoose.connect(mongoURL, () => {
