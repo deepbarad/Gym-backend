5a38898eb6dd0803d03e131e80d71e2c961fbd8d
deepbarad <deepbarad8@gmail.com>
2024-01-10 17:36:39 +0530

    change mongo url

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURL = "mongodb+srv://deepbarad8:5Gdn2P43OepPoHO8@hari-om-gym.xeigk0p.mongodb.net/"
+const mongoURL = "mongodb+srv://deepbarad8:k6NhSxvzOZwWn82P@cluster0.utywyap.mongodb.net/"
 mongoose.set('strictQuery', true);
 const connectToMongo = () => {
     mongoose.connect(mongoURL, () => {
