9083eb986f876a8ee31c4df124280132b6ca2e19
deepbarad <deepbarad8@gmail.com>
2024-01-10 11:03:55 +0530

    mongodb atlas

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURL = "mongodb://127.0.0.1:27017/gym"
+const mongoURL = "mongodb+srv://deepbarad8:5Gdn2P43OepPoHO8@hari-om-gym.xeigk0p.mongodb.net/"
 mongoose.set('strictQuery', true);
 const connectToMongo = () => {
     mongoose.connect(mongoURL, () => {
